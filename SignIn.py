1877223b92b71b586a3ef9985fcb0c4088d3b9d9
forgiveme0 <85843965+forgiveme0@users.noreply.github.com>
2021-09-11 00:45:24 +0800

    Update SignIn.py

--- SignIn.py
+++ SignIn.py
@@ -20,7 +20,7 @@ UA = "Mozilla/5.0 (Linux; Android 10; M2007J3SC Build/QKQ1.191222.002; wv) Apple
 def push(title, content):
     print(title+"\n"+content)
     url = "https://www.pushplus.plus/send"
-    data = { 'token':PUSHPLUS_TOKEN, 'title':title, 'content':content }
+    data = { 'token':BARK_TOKEN, 'title':title, 'content':content }
     # 发送请求
     res = requests.post(url=url, data=data).text
     # 输出发送结果
